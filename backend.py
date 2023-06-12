75289c906b1189981f998271dc600546593134c2
Shimon <shimonkocor@gmail.com>
2023-06-12 11:45:47 +0100

    Created a ChatGPT GUI with working Open API

---
+++ backend.py
@@ -0,0 +1,21 @@
+import openai
+
+
+class Chatbot:
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("Write a joke about birds")
+    print(response)
